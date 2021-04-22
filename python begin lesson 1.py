6c46c1f1bef0d68963113fe485af5b127f4a0d3e
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-04-22 20:49:05 +0200

    test branch for homework

---
+++ python begin lesson 1.py
@@ -0,0 +1,13 @@
+first_number = 1
+print(first_number)
+second_number = 5
+print(second_number)
+numbers = [1, 2, 3]
+print(numbers)
+str = 'программа'
+print(str)
+userNumber = input('Введите число: ')
+print(userNumber)
+userStr = input('Введите строку: ')
+print(userNumber)
+print(userNumber + userStr)
