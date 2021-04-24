f3fb0e6bb251241badcb5867491fa0a86de1f666
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-04-24 20:29:53 +0200

    Update python begin lesson 4.py
    исправлены имена переменных

--- python begin lesson 4.py
+++ python begin lesson 4.py
@@ -1,19 +1,19 @@
-userNumber = input('Введите целое положительное число')
-maxNumber = int(userNumber[0])
-for i in range(len(userNumber) - 1):
-    if maxNumber <= int(userNumber[i + 1]):
-        maxNumber = int(userNumber[i + 1])
-print(maxNumber)
+user_number = input('Введите целое положительное число')
+max_number = int(user_number[0])
+for i in range(len(user_number) - 1):
+    if max_number <= int(user_number[i + 1]):
+        max_number = int(user_number[i + 1])
+print(max_number)
 # второй вариант решения
-userNumber = input('Введите целое положительное число ')
-length = len(userNumber)
-userNumber = int(userNumber)
-maxNumber = userNumber % 10
-newNumber = userNumber // 10
+user_number = input('Введите целое положительное число ')
+length = len(user_number)
+user_number = int(user_number)
+max_number = user_number % 10
+new_number = user_number // 10
 for i in range(length - 1):
-    if newNumber % 10 > maxNumber:
-        maxNumber = newNumber % 10
-        newNumber = newNumber // 10
+    if new_number % 10 > max_number:
+        max_number = new_number % 10
+        new_number = new_number // 10
     else:
-        newNumber = newNumber // 10
-print(maxNumber)
+        new_number = new_number // 10
+print(max_number)
