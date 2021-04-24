eaca9b209104b4d39fa079a8a5188592a0fcc755
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-04-24 20:31:48 +0200

    Update python begin lesson 5.py
    Исправлены имена переменных
    Исправлены формулы рентабельности

--- python begin lesson 5.py
+++ python begin lesson 5.py
@@ -1,9 +1,9 @@
 profit = int(input('Введите значение прибыли: '))
 expense = int(input('Введите значение расходов: '))
 if profit > expense:
-    money = (profit - expense) / expense
+    money = (profit - expense) / profit
     print(f'Фирма приносит прибыль. Рентабельность: {round(money, 3)}')
-    numberWorkers = int(input('Введите количество работников: '))
-    print(f'Прибыль на 1 сотрудника составляет: {round(profit / numberWorkers, 3)}')
+    number_workers = int(input('Введите количество работников: '))
+    print(f'Прибыль на 1 сотрудника составляет: {round(profit - expense / number_workers, 3)}')
 else:
     print('Фирма работает в убыток')
