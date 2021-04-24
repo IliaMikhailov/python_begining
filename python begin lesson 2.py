ce91a9eb87189a9b0c7b52985beca7e6696c8196
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-04-24 20:27:50 +0200

    Update python begin lesson 2.py
    Исправлены имена переменных
    Исправлена отображения 1 часа вместо 60 минут

--- python begin lesson 2.py
+++ python begin lesson 2.py
@@ -1,8 +1,8 @@
-userNumber = int(input('Введите время в секундах: '))
-seconds = userNumber % 60
-minutes = userNumber // 60
+user_number = int(input('Введите время в секундах: '))
+seconds = user_number % 60
+minutes = user_number // 60
 hours = 0
-if minutes > 60:
-    hours = userNumber // 3600
+if minutes > 59:
+    hours = user_number // 3600
     minutes = minutes - hours * 60
 print(f'{hours:02d}:{minutes:02d}:{seconds:02d}')
