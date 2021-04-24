360f5985e1c81b1d984d2df68627fdbe176c0b65
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-04-24 20:28:31 +0200

    Update python begin lesson 3.py
    исправлены имена переменных

--- python begin lesson 3.py
+++ python begin lesson 3.py
@@ -1,6 +1,6 @@
-userNumber = input('Введите число ')
-print(type(userNumber))
-number_1 = int(userNumber)
+user_number = input('Введите число ')
+print(type(user_number))
+number_1 = int(user_number)
 number_2 = int(str(number_1) + str(number_1))
 number_3 = int(str(number_1) + str(number_2))
 sum = number_1 + number_2 + number_3
