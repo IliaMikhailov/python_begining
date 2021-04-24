7490acd1f881cca745501d3b91318304a2858a46
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-04-24 20:32:47 +0200

    Update python begin lesson 6.py
    Исправлен счётчик дней
    Исправлены имена переменных

--- python begin lesson 6.py
+++ python begin lesson 6.py
@@ -1,7 +1,7 @@
 start = int(input('Введите количество километров, которые пробегает спортсмен '))
-maxDistance = int(input('Введите к какому результату должен стремиться спортсмен '))
-count = 0
-while maxDistance > start:
+max_distance = int(input('Введите к какому результату должен стремиться спортсмен '))
+count = 1
+while max_distance > start:
     start *= 1.1
     count += 1
 print(f'За {count} дней спортсмен достигнет нужного результата')
