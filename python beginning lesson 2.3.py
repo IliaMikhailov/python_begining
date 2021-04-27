67bdcb1503903355debb4a7382ba1318e4a3e980
IliaMikhailov <82019331+IliaMikhailov@users.noreply.github.com>
2021-04-27 20:46:02 +0200

    Update python beginning lesson 2.3.py

--- python beginning lesson 2.3.py
+++ python beginning lesson 2.3.py
@@ -9,7 +9,7 @@ elif user_number == 6 or user_number == 7 or user_number == 8:
 elif user_number == 9 or user_number == 10 or user_number == 11:
     print(season_list[3])
 
-# Решение с изпользованием словаря
+# Решение с использованием словаря
 
 season_list_dict = {1:'зима', 2:'зима', 3:'весна', 4:'весна', 5:'весна', 6:'лето', 7:'лето', 8:'лето', 9:'осень', 10:'осень', 11:'осень', 12:'зима'}
 print(season_list_dict[user_number])
